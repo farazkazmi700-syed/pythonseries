7c3d6e79b52e2eaca2d97e3a06aab3023aa83903
Your Name <fzkazmi700@gmail.com>
2026-03-27 05:52:10 -0700

    Practice series of python

---
+++ quiz1.py
@@ -0,0 +1,19 @@
+## Question-03
+#initally value assign by str we have to convert the type of date through type casting method
+amount = input("Enter your salary: ")
+## type casting 
+amountx = int(input("Enter your salary: "))
+print(amount, type(amount))
+print(amountx, type(amountx))
+
+## Question-04
+#Primtive Data types : In this type we store single value of data 
+int=4
+float = 5.6
+str =  "Faraz"
+#Non-Primtive Data types : In this type we store multiple items of data 
+nums = [2,4,5,6,7,8] ##list
+tup =  (2,4,5) ## tuple
+set =  {3,4,6,4,54,54,55,43,23} ##set
+dic = {"Name":"faraz","age":24, "city":"lahore"}
+print(dic)
